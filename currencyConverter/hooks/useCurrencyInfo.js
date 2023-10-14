293205a11a354d040823517fdd2b6f9ef1720ac4
Abhishek Shukla <50260867+abhishek-shukla21@users.noreply.github.com>
2023-10-14 13:38:41 +0530

    added link and currency converter

---
+++ currencyConverter/hooks/useCurrencyInfo.js
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency){
+    
+}
+// jab bhi koi hook banao to uske aage use lga do unspoken rule.
---
+++ readme.md
@@ -0,0 +1,6 @@
+Projects URLs
+
+- Background Changer
+- Currency Converter
+- React Counter
+- [Password Generator](https://aesthetic-druid-0025b6.netlify.app/)
